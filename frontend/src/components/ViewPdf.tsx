56fe25826371b7ec672015d39dccce95ef3b8404
sr <7518647+wendychaung@users.noreply.github.com>
2022-12-16 02:41:34 +0800

    Update ViewPdf.tsx

--- frontend/src/components/ViewPdf.tsx
+++ frontend/src/components/ViewPdf.tsx
@@ -1,18 +1,30 @@
 import styled from "styled-components";
 import demo from "../assets/123.pdf"
 
+const Box = styled.div`
+  display: flex;
+  align-items: stretch;
+  justify-content: space-between;
+`
+
 const IframeBox = styled.div`
+flex-grow: 1;
   iframe{
     width: 100%;
     height: 100vh;
   }
 `
+const Rht = styled.div`
+  width: 250px;
+  color: #000000;
+  background: #fff;
+`
 
 export default function ViewPdf(){
-  return <div>
+  return <Box>
     <IframeBox>
       <iframe id="iframe"   src={`/pdfviewer/web/viewer.html?file=${demo}`} />
     </IframeBox>
-
-  </div>
+    <Rht>dd</Rht>
+  </Box>
 }
