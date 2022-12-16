c0ad3e062772d13613d375821a3e47b34cfa3f1b
sr <7518647+wendychaung@users.noreply.github.com>
2022-12-16 16:58:46 +0800

    Update ViewPdf.tsx

--- frontend/src/components/ViewPdf.tsx
+++ frontend/src/components/ViewPdf.tsx
@@ -18,31 +18,45 @@ flex-grow: 1;
 const Rht = styled.div`
   width: 250px;
   color: #000000;
-  background: #fff;
+  background: #f1f2f7;
+  box-sizing: border-box;
+  padding: 40px;
 `
 
+const SignBox = styled.div`
+  border-radius: 4px;
+  height: 54px;
+  width: 180px;
+  border: 1px dashed #3198f9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background: linear-gradient(120deg,#3198f910 , #00c1ff30);
+`
+
+
+
 export default function ViewPdf(){
+
+  const handleSign = () =>{
+    (document.querySelector('#iframe') as any).contentWindow.showSignature()
+  }
+
   return <Box>
     <IframeBox>
       <iframe id="iframe"   src={`/pdfviewer/web/viewer.html?file=${demo}`} />
     </IframeBox>
     <Rht>
-      <div
-           onClick={() => {
-             (document.querySelector('#iframe') as any).contentWindow.showSignature()
-             // (document.querySelector('#iframe') as any).contentWindow.gotoPageFrom(2)
-           }}
-      >
-        My Signatures
-      </div>
-      <div
-           onClick={() => {
+      <SignBox onClick={() => {handleSign()}}>Add My Signatures</SignBox>
+      {/*<div*/}
+      {/*     onClick={() => {*/}
 
-             (document.querySelector('#iframe') as any).contentWindow.gotoPageFrom(2)
-           }}
-      >
-        list
-      </div>
+      {/*       (document.querySelector('#iframe') as any).contentWindow.gotoPageFrom(2)*/}
+      {/*     }}*/}
+      {/*>*/}
+      {/*  list*/}
+      {/*</div>*/}
     </Rht>
   </Box>
 }
