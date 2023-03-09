3ac72344cfcc74a884ca79c9691164a325a9c7d8
sr <7518647+wendychaung@users.noreply.github.com>
2023-03-09 10:26:59 +0800

    update step

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,4 +1,4 @@
 window.configInfo = {
-    NODE_ADDRESS:"wss://node-workshop.gear.rs",
-    PROGRAM_ID:"0x8027b37269edce5715d246289aa86e031c645b8ed028bf4f4935c3b1dff36083"
+    NODE_ADDRESS:"wss://rpc-node.gear-tech.io",
+    PROGRAM_ID:"0x20d2727aff660c31e636ce2b4afb2672dd68a2d4f8753de4b8b7c4c6e164083e"
 }
